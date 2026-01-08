5235d2ab7e446483848d3ce87982bb351463bba0
sunil chanagoudanavar <sunilchangoudanaver@gmail.com>
2026-01-08 21:55:54 +0530

    tic-tok-game

--- tic-toc-game.js
+++ tic-toc-game.js
@@ -1,6 +1,9 @@
 let boxes=document.querySelectorAll('.box');
+let resetBtn = document.querySelector(".reset");
+let newGameBtn = document.querySelector(".newgame");
+
 let turnX=true;
-const winnerpattern=[
+const winnerPatterns=[
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -10,6 +13,7 @@ const winnerpattern=[
     [0,4,8],
     [2,4,6],
 ];
+
 
 boxes.forEach((box)=>{
     box.addEventListener("click",() =>{
@@ -23,6 +27,31 @@ boxes.forEach((box)=>{
         turnX=true;
       }
       box.disabled = true;
+      checkWinner();
 });    
 });
+const checkWinner = () => {
+  for (let pattern of winnerPatterns) {
 
+    let val1 = boxes[pattern[0]].innerText;
+    let val2 = boxes[pattern[1]].innerText;
+    let val3 = boxes[pattern[2]].innerText;
+
+    if (val1 !== "" && val1 === val2 && val2 === val3) {
+      alert("Winner is " + val1);
+    }
+
+  }
+};
+const resetGame = () => {
+    turnX = true;
+
+    boxes.forEach((box) => {
+        box.innerText = "";
+        box.disabled = false;
+    });
+};
+
+
+resetBtn.addEventListener("click", resetGame);
+newGameBtn.addEventListener("click", resetGame);

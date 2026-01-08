fe7ebb7b2967d14b2df6d213b74b8858bc68d9fa
sunil chanagoudanavar <sunilchangoudanaver@gmail.com>
2026-01-08 21:09:59 +0530

    tic-tok-game

--- tic-toc-game.js
+++ tic-toc-game.js
@@ -1,1 +1,28 @@
+let boxes=document.querySelectorAll('.box');
+let turnX=true;
+const winnerpattern=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",() =>{
+      console.log("bocx clicked");
+      if(turnX){
+        box.innerText="X"
+        turnX=false;
+      }
+      else{
+        box.innerText="O";
+        turnX=true;
+      }
+      box.disabled = true;
+});    
+});
 

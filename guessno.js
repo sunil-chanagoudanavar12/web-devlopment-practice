98ff47bf9937758ba48b4759081c420df3f75957
sunil chanagoudanavar <sunilchangoudanaver@gmail.com>
2026-01-12 23:04:01 +0530

    gusee the number

---
+++ guessno.js
@@ -0,0 +1,21 @@
+// random number between 1 to 10
+let a = Math.floor(Math.random() * 10) + 1;
+console.log(a); // testing purpose
+let test=0;
+let b;
+
+while (true) {
+    b = Number(prompt("Guess the number between 1 to 10"));
+    test++;
+
+    if (a === b) {
+        alert(`🎉 Congratulations! You guessed the correct number.  ${100-test}`);
+        break;
+    } 
+    else if (b < a) {
+        alert("Your number is smaller than the given number");
+    } 
+    else {
+        alert("Your number is bigger than the given number");
+    }
+}
